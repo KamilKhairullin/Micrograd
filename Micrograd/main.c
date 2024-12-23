5fd726b640091e83f16c9a4072a762a3ffb03019
Kamil Khairullin <kamil.khayrullin.personal@yandex.ru>
2024-12-23 23:24:38 +0300

    Initial Commit

---
+++ Micrograd/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Micrograd
+//
+//  Created by Kamil on 23.12.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
