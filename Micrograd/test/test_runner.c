f2f5a68aad67c18f44c3cbabc6f8e30d6b17022e
Kamil Khairullin <kamil.khayrullin.personal@yandex.ru>
2024-12-24 01:38:43 +0300

    added test to makefile

---
+++ Micrograd/test/test_runner.c
@@ -0,0 +1,8 @@
+//
+//  test_runner.c
+//  Micrograd
+//
+//  Created by Kamil on 24.12.2024.
+//
+
+#include "test_runner.h"
---
+++ Micrograd/test/test_runner.h
@@ -0,0 +1,13 @@
+//
+//  test_runner.h
+//  Micrograd
+//
+//  Created by Kamil on 24.12.2024.
+//
+
+#ifndef test_runner_h
+#define test_runner_h
+
+#include <stdio.h>
+
+#endif /* test_runner_h */
